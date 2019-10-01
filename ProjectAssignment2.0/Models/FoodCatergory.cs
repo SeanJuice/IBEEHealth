f77514aabf49a88cce65b6a149baef7e0f124dee
Siphokuhle Matshikwe <Siphokuhle Matshikwe@LAPTOP-TG4SSA02>
2019-10-01 20:17:31 +0200

    Province and Food Category Model

---
+++ ProjectAssignment2.0/Models/FoodCatergory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectAssignment2._0.Models
+{
+    public class FoodCatergory
+    {
+        private int FoodCategoryID;
+        private string FoodCatergory;
+        
+    }
+}
---
+++ ProjectAssignment2.0/Models/Province.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectAssignment2._0.Models
+{
+    public class Province
+    {
+        private int ProvinceID;
+        private string Name;
+    }
+}
