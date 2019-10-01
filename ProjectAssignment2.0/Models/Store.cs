9d84a8f6ba4930e6777c65f4233de71bf0d67f76
Mxolisi112 <mxolisi.dolophini@gmail.com>
2019-10-01 20:05:54 +0200

    Store and UserType Model

---
+++ ProjectAssignment2.0/Models/Store.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectAssignment2._0.Models
+{
+    public class Store
+    {
+        private int mStoreID;
+        private string mName;
+        private string mWebsite;
+        private bool mAvailableOnline;
+
+        public Store()
+        { }
+    }
+}
