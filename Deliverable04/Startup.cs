abde3e75a022a6d2e71cf9b15468421a980a22ff
Mxolisi112 <mxolisi.dolophini@gmail.com>
2019-10-16 19:28:10 +0200

    Adding the new local files

---
+++ Deliverable04/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Deliverable04.Startup))]
+namespace Deliverable04
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
